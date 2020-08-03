13c54e6bf892c5ed562cd7d7953ad9e785a4cd3a
Jacob Grow <jacobagrow@gmail.com>
2020-08-03 10:43:19 -0600

    Style update

--- src/App.js
+++ src/App.js
@@ -51,30 +51,31 @@ deleteItem(id){
       <div className="App">
         <div className="container-fluid">
           <div className="row mt-5">
-            <div className="col-6 mx-auto">
+            <div className="col-6 mx-auto text-center">
 
 
 
 
         <div>
-        Add an Item...
+       <h1 className="">Make A List</h1>
         <br/>
+        
         <input className="form-control"
           type="text"
           placeholder="Type item here..."
           value={this.state.newItem}
-          onChange={e => this.updateInput("newItem", e.target.value)} />
-          <button className="btn btn-secondary"
+          onChange={e => this.updateInput("newItem", e.target.value)}/>
+          <button className="btn btn-secondary mt-3 btn-lg"
             onClick={() => this.addItem()}
             >
               Add
             </button>
             <br/>
-            <ul>
+            <ol>
               {this.state.list.map(item => {
                 return(
                   <li key={item.id}>
-                      {item.value}
+                     <h4> {item.value} </h4>
                       <button className="btn btn-primary" onClick={() => this.deleteItem(item.id)}>  
                       X
                       </button>
@@ -82,7 +83,7 @@ deleteItem(id){
                 )
               })}
 
-            </ul>
+            </ol>
               </div>
         </div>
       </div>
@@ -90,6 +91,7 @@ deleteItem(id){
                 </div>
     )
   }
+
 
 
 
